021538a38d0eb966ee8b695183674b8ee1eee97a
Matt Levine <mtlevine0@gmail.com>
2021-06-04 00:31:13 -0400

    Sanitize request path

--- src/main/java/com/mtlevine0/HttpThread.java
+++ src/main/java/com/mtlevine0/HttpThread.java
@@ -2,6 +2,8 @@ package com.mtlevine0;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.logging.Logger;
@@ -17,15 +19,18 @@ public class HttpThread implements Runnable {
 
     @Override
     public void run() {
+        OutputStream out = null;
+        InputStream in = null;
         try {
             LOGGER.info("Running thread!");
-            OutputStream out = socket.getOutputStream();
-            InputStream in = socket.getInputStream();
+            out = socket.getOutputStream();
+            in = socket.getInputStream();
             String rawRequest = readRequest(in);
             HttpRequest request = new HttpRequest(rawRequest);
             LOGGER.info(request.toString());
 
-            byte[] bytes = Files.readAllBytes(Paths.get("src/main/resources" + request.getPath()));
+            String sanitizedPath = new URI(request.getPath()).normalize().getPath();
+            byte[] bytes = Files.readAllBytes(Paths.get("src/main/resources" + sanitizedPath));
             HttpResponse httpResponse = HttpResponse.builder()
                     .status(HttpStatus.OK)
                     .protocolVersion("HTTP/1.1")
@@ -33,11 +38,19 @@ public class HttpThread implements Runnable {
                     .build();
 
             out.write(httpResponse.getResponse());
-            in.close();
-            out.close();
-            socket.close();
+
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+                out.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
