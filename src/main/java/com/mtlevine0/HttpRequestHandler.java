cfd53300d71bd27002c11fb097e40415c5a2d15c
Matt Levine <mtlevine0@gmail.com>
2021-06-24 03:17:17 -0400

    Added directory listing

--- src/main/java/com/mtlevine0/HttpRequestHandler.java
+++ src/main/java/com/mtlevine0/HttpRequestHandler.java
@@ -5,6 +5,7 @@ import com.mtlevine0.request.HttpMethod;
 import com.mtlevine0.request.HttpRequest;
 import com.mtlevine0.response.HttpResponse;
 import com.mtlevine0.response.HttpStatus;
+import org.apache.commons.io.FileUtils;
 
 import java.io.*;
 import java.net.Socket;
@@ -14,6 +15,9 @@ import java.nio.file.AccessDeniedException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class HttpRequestHandler implements Runnable {
@@ -23,14 +27,14 @@ public class HttpRequestHandler implements Runnable {
     private OutputStream out;
     private InputStream in;
 
+    private static final String BASE_PATH = "../httpj";
+
     public HttpRequestHandler(Socket socket) {
         this.socket = socket;
         try {
             out = socket.getOutputStream();
             in = socket.getInputStream();
-        } catch (IOException e) {
-
-        }
+        } catch (IOException e) { }
     }
 
     @Override
@@ -40,8 +44,12 @@ public class HttpRequestHandler implements Runnable {
         try {
             HttpRequest request = new HttpRequest(in);
             if (request.getMethod().equals(HttpMethod.GET)) {
-                body = loadResource(request.getPath());
                 httpStatus = HttpStatus.OK;
+                if (!isDirectory(request.getPath())) {
+                    body = loadResource(request.getPath());
+                } else {
+                    body = lsDir(request.getPath()).getBytes();
+                }
             } else if (request.getMethod().equals(HttpMethod.HEAD)) {
                 httpStatus = HttpStatus.OK;
             } else {
@@ -73,11 +81,37 @@ public class HttpRequestHandler implements Runnable {
         }
     }
 
+    private String lsDir(String path) throws URISyntaxException {
+        String sanitizedPath = new URI(path).normalize().getPath();
+        List<File> files = Arrays.asList(new File(BASE_PATH + sanitizedPath).listFiles());
+        StringBuilder sb = new StringBuilder();
+        sb.append("<table>");
+        sb.append("<tr><th>Name</th><th>Last Modified</th><th>Size</th></tr>");
+        for (File file : files) {
+            sb.append("<tr>");
+            sb.append("<td>" + file.getName());
+            if (file.isDirectory()) {
+                sb.append("/");
+            }
+            sb.append("</td>");
+            sb.append("<td>" + Instant.ofEpochMilli(file.lastModified()).toString() + "</td>");
+            sb.append("<td>" + FileUtils.byteCountToDisplaySize(file.length()) + "</td>");
+            sb.append("</tr>");
+        }
+        sb.append("</table>");
+        return sb.toString();
+    }
+
+    private boolean isDirectory(String path) throws URISyntaxException {
+        String sanitizedPath = new URI(path).normalize().getPath();
+        return new File(BASE_PATH + sanitizedPath).isDirectory();
+    }
+
     private byte[] loadResource(String path) throws URISyntaxException, NoSuchFileException, AccessDeniedException {
         byte[] resource = null;
         try {
             String sanitizedPath = new URI(path).normalize().getPath();
-            resource = Files.readAllBytes(Paths.get("src/main/resources" + sanitizedPath));
+            resource = Files.readAllBytes(Paths.get(BASE_PATH + sanitizedPath));
         } catch (NoSuchFileException | URISyntaxException |AccessDeniedException e) {
             throw e;
         } catch (IOException e) {
