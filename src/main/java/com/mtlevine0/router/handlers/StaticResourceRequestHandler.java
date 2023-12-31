b898163a5b186f73a460ef4fd8aa8ab5ab177217
Matt Levine <mtlevine0@gmail.com>
2023-12-31 00:05:05 -0500

    Add Picocli support for StaticServer class.  Remove FeatureFlag toggle from StaticResourceRequestHandler.

--- src/main/java/com/mtlevine0/router/handlers/StaticResourceRequestHandler.java
+++ src/main/java/com/mtlevine0/router/handlers/StaticResourceRequestHandler.java
@@ -1,7 +1,5 @@
 package com.mtlevine0.router.handlers;
 
-import com.mtlevine0.httpj.FeatureFlag;
-import com.mtlevine0.httpj.FeatureFlagContext;
 import com.mtlevine0.httpj.common.RequestHandler;
 import com.mtlevine0.httpj.common.request.HttpRequest;
 import com.mtlevine0.router.utils.DirectoryUtil;
@@ -9,8 +7,6 @@ import com.mtlevine0.router.utils.ResourceUtil;
 import com.mtlevine0.httpj.common.response.HttpResponse;
 import com.mtlevine0.httpj.common.response.HttpStatus;
 import lombok.SneakyThrows;
-
-import java.nio.file.NoSuchFileException;
 
 public class StaticResourceRequestHandler implements RequestHandler {
 
@@ -25,11 +21,7 @@ public class StaticResourceRequestHandler implements RequestHandler {
     public void handleRequest(HttpRequest request, HttpResponse response) {
         byte[] body;
         if (ResourceUtil.isDirectory(basePath, request.getPath())) {
-            if (FeatureFlagContext.getInstance().isFeatureActive(FeatureFlag.DIRECTORY_LISTING)) {
-                body = DirectoryUtil.lsDir(basePath, request.getPath()).getBytes();
-            } else {
-                throw new NoSuchFileException("File Does Not Exist: " + request.getPath());
-            }
+            body = DirectoryUtil.lsDir(basePath, request.getPath()).getBytes();
         } else {
             body = ResourceUtil.loadResource(basePath, request.getPath());
         }
